9950982cb08e001251c0f0cd470d6727792109ba
Adrien Folie <folie.adrien@gmail.com>
2016-01-26 15:16:03 +0100

    add tests for user serializer

---
+++ spec/serializers/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe UserSerializer, :type => :serializer do
+
+  context 'Individual Resource Representation' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    let(:serialized) do
+      Serialized.new(UserSerializer.new(user))
+    end
+
+    it 'serializes the specified attributes' do
+      expect(serialized.attributes).to have_serialized_attributes(
+        user.slice(:first_name, :last_name, :birthday, :email, :phone_number)
+      )
+    end
+  end
+end
