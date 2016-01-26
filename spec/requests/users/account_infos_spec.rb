4d1d5bd04a4b07c0d63e660e404673ab4ea601ea
Adrien Folie <folie.adrien@gmail.com>
2016-01-26 14:42:57 +0100

    add action for user infos

---
+++ spec/requests/users/account_infos_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Users account infos', :type => :request do
+  before do
+    get users_path, headers: json_headers
+  end
+
+  it_behaves_like 'an action requiring authentication'
+
+  context 'when user is authenticated' do
+    let(:user)          { FactoryGirl.create(:user) }
+    let(:authenticated) { true }
+
+    it { is_expected.to return_status_code 200 }
+
+    it 'returns the user attributes' do
+      expect(response.body).to equal_serialized(user)
+    end
+  end
+end
