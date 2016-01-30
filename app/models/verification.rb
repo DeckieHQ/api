ae5939aa73d460061e0dfbda2fc251e603578a33
Adrien Folie <folie.adrien@gmail.com>
2016-01-30 07:43:08 +0100

    improves verification error messages

--- app/models/verification.rb
+++ app/models/verification.rb
@@ -3,6 +3,14 @@ class Verification
 
   attr_accessor :type, :token, :model
 
+  validates :type, inclusion: { in: %w(email phone_number) }
+
+  validate :model_attribute_must_be_speficied, on: :send_instructions
+
+  validate :model_attribute_must_be_unverified, on: [:send_instructions, :complete]
+
+  validate :token_must_be_valid, on: :complete
+
   def initialize(attributes = {}, model: nil)
     attributes = attributes || {}
 
@@ -11,14 +19,6 @@ class Verification
     @token =  attributes[:token] || ''
     @model = model
   end
-
-  validates :type, inclusion: { in: %w(email phone_number) }
-
-  validate :model_attribute_must_be_speficied, on: :send_instructions
-
-  validate :model_attribute_must_be_unverified, on: [:send_instructions, :complete]
-
-  validate :token_must_be_valid, on: :complete
 
   def send_instructions
     return false unless valid? && valid?(:send_instructions)
@@ -38,13 +38,13 @@ class Verification
   def model_attribute_must_be_speficied
     model_attribute = @model.send("#{type}")
 
-    errors.add(:base, :blank) if model_attribute.nil?
+    errors.add(:base, :unspecified, base_error_options) unless model_attribute
   end
 
   def model_attribute_must_be_unverified
     attribute_verified_at = @model.send("#{@type}_verified_at")
 
-    errors.add(:base, :already_verified) unless attribute_verified_at.nil?
+    errors.add(:base, :already_verified, base_error_options) if attribute_verified_at
   end
 
   def token_must_be_valid
@@ -57,5 +57,9 @@ class Verification
 
   def sent_at
     @model.send("#{@type}_verification_sent_at")
+  end
+
+  def base_error_options
+    { target: @model.class.name.to_sym, type: @type }
   end
 end
