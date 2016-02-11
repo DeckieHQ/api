ef1fb8177d97476fae0cce75cf2acf106bcaf1d2
Forster Groux <groux.forster@gmail.com>
2016-02-11 19:11:05 +0100

    replace add_reference by t.references

--- db/migrate/20160203162506_create_profiles.rb
+++ db/migrate/20160203162506_create_profiles.rb
@@ -6,15 +6,13 @@ class CreateProfiles < ActiveRecord::Migration[5.0]
       t.text   :short_description
       t.text   :description
 
+      t.references :profiles, :user, index: { unique: true }, foreign_key: true
+
       t.timestamps null: false
     end
-
-    add_reference :profiles, :user, index: { unique: true }, foreign_key: true
   end
 
   def down
-    remove_reference :profiles, :user, index: { unique: true }, foreign_key: true
-
     drop_table :profiles
   end
 end
