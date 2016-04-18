a346927170d5d77c009c2594eaa2b12ed7a873fd
Forster Groux <groux.forster@gmail.com>
2016-04-18 12:12:19 +0200

    remove unused links from serializer

--- app/controllers/event/submissions_controller.rb
+++ app/controllers/event/submissions_controller.rb
@@ -2,7 +2,7 @@ class Event::SubmissionsController < ApplicationController
   before_action :authenticate!, only: [:show]
 
   def show
-    render json: event.submission_of(current_user)
+    render json: event.submission_of(current_user) || { data: nil }
   end
 
   protected
