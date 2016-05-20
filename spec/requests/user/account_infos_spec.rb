541f295ff10ae93b1cb4a15b710548738da204f5
Adrien Folie <folie.adrien@gmail.com>
2016-05-20 15:53:15 +0200

    allow get user to include profile

--- spec/requests/user/account_infos_spec.rb
+++ spec/requests/user/account_infos_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'User account infos', :type => :request do
+  let(:params) {}
+
   before do
-    get user_path, headers: json_headers
+    get user_path, params: params, headers: json_headers
   end
 
   it_behaves_like 'an action requiring authentication'
@@ -16,5 +18,7 @@ RSpec.describe 'User account infos', :type => :request do
     it 'returns the user attributes' do
       expect(response.body).to equal_serialized(user)
     end
+
+    it_behaves_like 'an action with include', :user, accept: %w(profile), on: :member
   end
 end
