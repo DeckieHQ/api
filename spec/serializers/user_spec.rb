4597b3487bb63d60d12ae69aacdca30e645f0c51
Adrien Folie <folie.adrien@gmail.com>
2016-06-16 18:04:22 +0200

    add missing links on user

--- spec/serializers/user_spec.rb
+++ spec/serializers/user_spec.rb
@@ -24,8 +24,10 @@ RSpec.describe UserSerializer, :type => :serializer do
       expect(serialized).to have_relationship_link_for(:profile, target: user.profile)
     end
 
-    it 'adds the hosted events link' do
-      expect(serialized).to have_relationship_link_for(:hosted_events)
+    [:hosted_events, :submissions, :notifications].each do |link|
+      it "adds the #{link} link" do
+        expect(serialized).to have_relationship_link_for(link)
+      end
     end
   end
 end
